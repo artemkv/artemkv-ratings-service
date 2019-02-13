1aba21322566b2b6631cb77c8feb7364a400ea36
artemkv <artemkv@gmail.com>
2019-02-13 17:43:10 +0100

    Correct service paths

--- start/start-service.sh
+++ start/start-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/ubuntu/prod/artemkv-ratings-service/start/
+cd /home/ubuntu/prod/ratings/artemkv-ratings-service/start/
 NODE_IP= NODE_PORT=8333 PEER_URL=http://localhost:8334 forever start app.js
